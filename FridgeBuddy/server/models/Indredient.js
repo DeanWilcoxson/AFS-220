f7960f86f02fbbae24fe01e4ff8415e47278c042
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2022-02-27 18:09:00 -0800

    2/27/2022

--- FridgeBuddy/server/models/Indredient.js
+++ FridgeBuddy/server/models/Indredient.js
@@ -1,3 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const 
+const ingredientSchema = new Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  amount: { type: String, required: true },
+});
+module.exports = mongoose.model("ingredient", ingredientSchema);
